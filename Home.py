7eef763f8b7b3a237acfe78a39b7278aeaff8c5c
avi-lek <avi.lekkelapudi@gmail.com>
2024-12-28 00:32:10 -0800

    s

--- Home.py
+++ Home.py
@@ -1,11 +1,14 @@
 import streamlit as st
-from st_pages import hide_pages
+from st_pages import hide_pages, add_page_title, get_nav_from_toml
 from streamlit_extras.let_it_rain import rain 
 import toml
 
 #Configure Page
 st.set_page_config(page_title="My Protein Is Broken!", page_icon=":dna:", layout="wide")
 
+nav = get_nav_from_toml(".streamlit/pages.toml")
+pg = st.navigation(nav)
+#add_page_title(pg)
 hide_pages(["Transcription", "Identify Mutations", "Translation", "Sandbox Instructions"])
 st.header("Home")
 st.markdown("""
